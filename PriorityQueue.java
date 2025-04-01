f44b57d28b7ba15269f49118e1383428896e98a2
nhogg <nathanhogg1223@gmail.com>
2025-04-01 13:09:59 -0500

    Signed Header

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -1,7 +1,7 @@
 
 /******************************************************************
  *
- *   YOUR NAME / SECTION NUMBER
+ *   Nathan Hogg / Section 002
  *
  *   Note, additional comments provided throughout this source code
  *   is for educational purposes
